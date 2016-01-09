0aa80007fa078b447bb23f4ec69fb1bf8b80458b
Coy Humphrey <cmhumphr@ucsc.edu>
2016-01-09 13:17:13 -0800

    Opens/closes files. Checks for errors

--- asg0/cat.c
+++ asg0/cat.c
@@ -1,8 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main (int argc, char **argv) {
+	char *exec_name = argv[0];
 	for (int i = 1; i < argc; ++i) {
-		printf ("%s\n", argv[i]);
+		int fd = open (argv[i], O_RDONLY);
+		if (fd < 0) {
+			perror(argv[i]);
+			return EXIT_FAILURE;
+		}
+
+		if(close (fd)) {
+			perror(exec_name);
+			return EXIT_FAILURE;
+		}
 	}
+	return EXIT_SUCCESS;
 }
