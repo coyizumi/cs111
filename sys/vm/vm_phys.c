c96ae5a735c7551d94957b6af3bde807aa3f5067
Timmy Ma Kwan <tmakwan@ucsc.edu>
2016-02-27 23:29:22 -0800

    Changed some stuff in vm_phys.c for freelist

--- sys/vm/vm_phys.c
+++ sys/vm/vm_phys.c
@@ -248,9 +248,12 @@ vm_freelist_add(struct vm_freelist *fl, vm_page_t m, int order, int tail)
 
 	m->order = order;
 	if (tail)
-		TAILQ_INSERT_TAIL(&fl[order].pl, m, plinks.q);
-	else
+		//Custom
+		//TAILQ_INSERT_TAIL(&fl[order].pl, m, plinks.q);
 		TAILQ_INSERT_HEAD(&fl[order].pl, m, plinks.q);
+	else
+		//TAILQ_INSERT_HEAD(&fl[order].pl, m, plinks.q);
+		TAILQ_INSERT_TAIL(&fl[order].pl, m, plinks.q);
 	fl[order].lcnt++;
 }
 
