78a741fc3d098a5a9cb9c04066a5d89e97a4a42d
jwvance <jwvance@ucsc.edu>
2016-03-08 17:07:27 -0800

    changes to setkey.c

--- sys/kern/sys_setkey.c
+++ sys/kern/sys_setkey.c
@@ -4,16 +4,47 @@
 #include <sys/types.h>
 #include <sys/systm.h>
 
-#ifndef _SYS_SYSPROTO_H_
+#include <sys/proc.h>
+#include <sys/ucred.h>
 
+//struct declarations
+#ifndef _SYS_SYSPROTO_H_
 struct setkey_args {
    unsigned int k0;
    unsigned int k1;
 }
-
 #endif
 
-int sys_setkey(struct thread *td, struct setkey_args *args) {
-   printf("Hello World\n");
-   return 0;
+struct user_keys {
+	uid_t user;
+	unsigned int k0;
+	unsigned int k1;
 }
+
+
+
+int sys_setkey(struct thread *td, struct setkey_args *args) {
+   
+	//check uid via ucred
+	uid_t userID;
+	user = (td->td_ucred->cr_uid);
+   
+	//malloc space for 20 users (need minimum of 16)
+	struct user_keys key_table[20];
+	
+	//fill out user_keys with relevant data
+	key_table->user = userID;
+	key_table->k0 = args->k0;
+	key_table->k1 = args->k1;
+   
+	printf("Hello World\n");
+	return 0;
+}
+
+
+
+
+
+
+
+
