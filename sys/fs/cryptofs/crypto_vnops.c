510303dd94ee6bcbdea83184e496f00ca68c11be
Coy Humphrey <cmhumphr@ucsc.edu>
2016-03-10 12:46:54 -0800

    Playing with read/write in crpytofs

--- sys/fs/cryptofs/crypto_vnops.c
+++ sys/fs/cryptofs/crypto_vnops.c
@@ -908,7 +908,7 @@ crypto_read (struct vop_read_args *ap)
 	{
 		printf ("crypto_read: uid: %d\n", ap->a_cred->cr_ruid);
 		printf ("crypto_read: ioflags: %o\n", ap->a_ioflag);
-		printf ("crypto_read: desc_flags: %o\n", ap->a_gen.vdesc_flags);
+		printf ("crypto_read: desc_flags: %o\n", ap->a_gen.a_desc->vdesc_flags);
 	}
 	int retval = crypto_bypass((struct vop_generic_args*) ap);
 	char buffer[256];
@@ -933,7 +933,7 @@ crypto_write (struct vop_write_args *ap)
 	{
 		printf ("crypto_write: uid: %d\n", ap->a_cred->cr_ruid);
 		printf ("crypto_write: ioflags: %o\n", ap->a_ioflag);
-		printf ("crypto_write: desc_flags: %o\n", ap->a_gen.vdesc_flags);
+		printf ("crypto_write: desc_flags: %o\n", ap->a_gen.a_desc->vdesc_flags);
 	}
 	struct uio *u = ap->a_uio;
 	char buffer[256];
