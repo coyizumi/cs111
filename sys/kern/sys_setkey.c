abe326515e26ef00b93f956434373f6e2d13ef10
Coy Humphrey <cmhumphr@ucsc.edu>
2016-03-10 22:46:45 -0800

    Playing with sys_setkey.c

--- sys/kern/sys_setkey.c
+++ sys/kern/sys_setkey.c
@@ -13,8 +13,10 @@
 struct setkey_args {
    unsigned int k0;
    unsigned int k1;
-}
+};
 #endif
+
+#define KEYSTORE_LENGTH 20
 
 //Already declared in the header file, so not needed??
 /*struct user_keys {
@@ -23,23 +25,62 @@ struct setkey_args {
 	unsigned int k1;
 }*/
 
+struct user_keys {
+	uid_t user;
+	unsigned int k0;
+	unsigned int k1;
+	int valid;
+};
 
+struct key_store {
+	struct user_keys keylist[KEYSTORE_LENGTH];
+	int len;
+};
+
+static struct key_store keys;
 
 int sys_setkey(struct thread *td, struct setkey_args *args) {
-   
 	//check uid via ucred
 	uid_t userID;
+	int i;
+	struct user_keys *key;
 	userID = (td->td_ucred->cr_uid);
-   
-	//malloc space for 20 users (need minimum of 16)
-	struct user_keys key_table[20];
+
+	for (i = 0; i < KEYSTORE_LENGTH; i++)
+	{
+		key = &(keys.keylist[i]);
+		if (!key->valid)
+			break;
+	}
 	
 	//fill out user_keys with relevant data
-	key_table->user = userID;
-	key_table->k0 = args->k0;
-	key_table->k1 = args->k1;
+	key->user = userID;
+	key->k0 = args->k0;
+	key->k1 = args->k1;
+	key->valid = 1;
+
+	for (i = 0; i < KEYSTORE_LENGTH; i++)
+	{
+		key = &(keys.keylist[i]);
+		printf ("uid: %d ; k0: %d ; k1: %d\n", key->uid, key->k0, key->k1);
+	}
    
 	printf("uid: %d\n", userID);
+	return 0;
+}
+
+int get_keys_by_uid (uid_t uid, int *k0, int *k1)
+{
+	int i;
+	for (i = 0; i < KEYSTORE_LENGTH; i++)
+	{
+		if (keys.keylist[i].valid && keys.keylist[i].user == uid)
+		{
+			*k0 = keys.keylist[i].k0;
+			*k1 = keys.keylist[i].k1;
+			return 1;
+		}
+	}
 	return 0;
 }
 
--- sys/sys/keys.h
+++ sys/sys/keys.h
@@ -1,15 +1,10 @@
 #ifndef SYS_H_KEYS
 #define SYS_H_KEYS
+#include <sys/types.h>
 /*
   Static array that holds values for users along with their corresponding keys
 */
 
-struct user_keys {
-	uid_t user;
-	unsigned int k0;
-	unsigned int k1;
-};
-
-typedef struct user_keys user;
+int get_keys_by_uid (uid_t uid, int *k0, int *k1);
 
 #endif
