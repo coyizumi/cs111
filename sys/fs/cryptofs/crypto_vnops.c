ecb7bbe78f235e2d19ee22605c36a94d9d779cd3
Coy Humphrey <cmhumphr@ucsc.edu>
2016-03-11 17:15:33 -0800

    Experimenting with read

--- sys/fs/cryptofs/crypto_vnops.c
+++ sys/fs/cryptofs/crypto_vnops.c
@@ -922,7 +922,7 @@ static void crypto_encrypt (struct uio *uio, int k0, int k1, long fileid, long l
 
   printf ("cyrpto_encrypt: IN ; size: %ld\n", length);
 
-  char *iov_base = (char*)(uio->uio_iovec->iov_base) - uio->uio_offset;
+  char *iov_base = (char*)(uio->uio_iov->iov_base) - uio->uio_offset;
   uio->uio_iov->iov_base = iov_base;
 
   /*
